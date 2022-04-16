b8c5af4cf7ce1675d8cd21ea6e8b571a29b51a0d
Mau5trakt <kubzalemany@hotmail.com>
2022-04-16 17:46:41 -0600

    CREATED function to get the ascii chacter from a number

--- pynumpad.py
+++ pynumpad.py
@@ -3,38 +3,40 @@ from sys import argv
 import pyperclip
 
 # Create the parser and add arguments
+favoritos = ["F1", "F2", "F3", "F4", "F5"]
+argumentos = ["FA" ,"FP", favoritos]
+error = "Usage: python pynumpad.py {number} or {option}\n" \
+        f"\targumentos: \n\t\t favoritos: \n \t\t\t{argumentos[2]}"
+
+
 print(f"number of arguments {len(sys.argv)}")
 if len(sys.argv) == 1:
-    print("Usage: python pynumpad.py {number} or {option}")
-
-argumentos = ["F1", "F2", "F3", "F4"]
+    print(error)
 
 
-favoritos = []
+
+
+
 for arg in argv[1:]:
+    arg = arg.lower()
+    f = open("favs.txt", "r")
+    doc = f.readline()
     print(arg)
     print(type(arg))
     if len(sys.argv) == 2 and arg[0].isnumeric():
-        print("Es numero")
-    elif arg in argumentos:
+        print(chr(int(arg)))
+
+    elif arg == "fp":
+        print(doc)
+    elif arg == "FA":
+        print("Saber como le voy a hacer para agregar uno nuevo xDDD")
+    elif arg in favoritos:
         a = arg[1]
-        #print(arg[1])
-        #print(f"Favorito {a}")
-        f = open("favs.txt", "r")
+        print(arg[1])
+        print(f"Favorito {a}")
         doc = f.readline()
         posicion = doc[int(a) - 1]
         print(doc[int(a) - 1])
         pyperclip.copy(posicion)
-        """
-        f = open("favs.txt", "r")
-        f1 = f.readline()
-        if arg == "F1":
-            print("Favorito 1")
-            print(f1[:1])
-            print(f1[2:3])
-            pyperclip.copy(f1[2:3])
-        elif arg == "F2":
-            print("Favorito 2")
-            print(f1[1])
-            pyperclip.copy(f1[2:3])"""
-
+    else:
+        print(error)
